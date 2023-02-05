9656f3d1f56976a500871ad3f6467a534b7a0993
roma_p <pelle.romain@gmail.com>
2023-02-05 22:00:26 +0100

    test_package fixed.

--- tests/test_package.py
+++ tests/test_package.py
@@ -1,96 +1,76 @@
-import shutil
 import unittest
 
-from tests.fake_ssh_connection import FakeSSHConnection
-from vit.connection.ssh_connection import SSHConnection
-from vit.connection.vit_connection import VitConnection
-from vit.connection.vit_connection import ssh_connect_auto
 from vit.custom_exceptions import *
 from vit.vit_lib import (
-    repo_init_clone,
     package, fetch
 )
+
+from tests import vit_test_repo as repo
+from vit.connection.vit_connection import ssh_connect_auto
 
 
 class TestPackage(unittest.TestCase):
 
-    test_origin_path_ok = "tests/origin_repo"
-    test_local_path_1 = "tests/local_repo1"
-    test_local_path_2 = "tests/local_repo2"
-    test_package = "the/package"
-
     def setUp(self):
-        VitConnection.SSHConnection = FakeSSHConnection
-        self._clean_dir()
-        self._init_repos()
+        repo.setup_test_repo("repo_empty")
 
     def tearDown(self):
-        VitConnection.SSHConnection = SSHConnection
-        self._clean_dir()
-
-    def _init_repos(self):
-        repo_init_clone.init_origin(self.test_origin_path_ok)
-        repo_init_clone.clone(
-            os.path.abspath(self.test_origin_path_ok),
-            self.test_local_path_1,
-            "romainpelle", "localhost"
-        )
-        repo_init_clone.clone(
-            os.path.abspath(self.test_origin_path_ok),
-            self.test_local_path_2,
-            "romainpelle", "localhost"
-        )
-
-    def _clean_dir(self):
-        for path in (
-                self.test_origin_path_ok,
-                self.test_local_path_1,
-                self.test_local_path_2):
-            self._rm_dir(path)
+        repo.dispose_test_repo()
 
     def test_create_package(self):
-        package.create_package(self.test_local_path_1, "path1")
+        with ssh_connect_auto(repo.test_local_path_1) as vit_connection:
+            package.create_package(
+                repo.test_local_path_1,
+                vit_connection,
+                "path1")
 
     def test_create_package_but_already_exists(self):
-        package.create_package(self.test_local_path_1, "path1")
-        with self.assertRaises(Package_AlreadyExists_E):
-            package.create_package(self.test_local_path_1, "path1")
-        with self.assertRaises(Package_AlreadyExists_E):
-            package.create_package(self.test_local_path_2, "path1")
+        with ssh_connect_auto(repo.test_local_path_1) as vit_connection:
+            package.create_package(
+                repo.test_local_path_1,
+                vit_connection, "path1"
+            )
+            with self.assertRaises(Package_AlreadyExists_E):
+                package.create_package(
+                    repo.test_local_path_1,
+                    vit_connection, "path1"
+                )
+        with ssh_connect_auto(repo.test_local_path_2) as vit_connection:
+            with self.assertRaises(Package_AlreadyExists_E):
+                package.create_package(
+                    repo.test_local_path_2,
+                    vit_connection, "path1"
+                )
 
     def test_create_package_but_parent_dir_does_not_exists(self):
         with self.assertRaises(Path_ParentDirNotExist_E):
-            package.create_package(self.test_local_path_1, "/not/exists")
+            with ssh_connect_auto(repo.test_local_path_1) as vit_connection:
+                package.create_package(
+                    repo.test_local_path_1,
+                    vit_connection, "/not/exists"
+                )
 
     def test_create_package_and_force_subtree(self):
-        package.create_package(
-            self.test_local_path_1,
-            "new/package",
-            force_subtree=True
-        )
+        with ssh_connect_auto(repo.test_local_path_1) as vit_connection:
+            package.create_package(
+                repo.test_local_path_1,
+                vit_connection,
+                "new/package",
+                force_subtree=True
+            )
 
     def test_list_packages(self):
-        package.create_package(self.test_local_path_1, "path1")
-        package.create_package(self.test_local_path_1, "path2")
-        with ssh_connect_auto(self.test_local_path_2) as ssh_connection:
-            fetch.fetch(self.test_local_path_2, ssh_connection)
+        with ssh_connect_auto(repo.test_local_path_1) as vit_connection:
+            package.create_package(repo.test_local_path_1, vit_connection, "path1")
+            package.create_package(repo.test_local_path_1, vit_connection, "path2")
+
+        with ssh_connect_auto(repo.test_local_path_2) as ssh_connection:
+            fetch.fetch(repo.test_local_path_2, ssh_connection)
         self.assertSetEqual(
             {"path1", "path2"},
-            set(package.list_packages(self.test_local_path_2))
+            set(package.list_packages(repo.test_local_path_2))
         )
 
-    @staticmethod
-    def _rm_dir(directory):
-        if os.path.exists(directory):
-            shutil.rmtree(directory, ignore_errors=True)
-
-if __name__ == '__main__':
-    unittest.main()
-
-    @staticmethod
-    def _rm_dir(directory):
-        if os.path.exists(directory):
-            shutil.rmtree(directory, ignore_errors=True)
 
 if __name__ == '__main__':
     unittest.main()
