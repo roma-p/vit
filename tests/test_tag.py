c84f71d4ae5a5026c410319c1f50660826cd2417
roma_p <pelle.romain@gmail.com>
2023-02-05 22:39:56 +0100

    test_tag fixed.

--- tests/test_tag.py
+++ tests/test_tag.py
@@ -1,176 +1,140 @@
-import shutil
+import time
 import unittest
 
-from vit.connection.vit_connection import VitConnection
-from vit.connection.vit_connection import ssh_connect_auto
 from vit.custom_exceptions import *
-from vit.vit_lib import (
-    repo_init_clone,
-    package, asset,
-    asset_template,
-    tag, branch,
-    fetch
-)
+from vit.vit_lib import (tag, branch, fetch)
 
-from vit.connection.ssh_connection import SSHConnection
-from tests.fake_ssh_connection import FakeSSHConnection
+from tests import vit_test_repo as repo
+from vit.connection.vit_connection import ssh_connect_auto
 
 
 class TestTag(unittest.TestCase):
 
-    test_origin_path_ok = "tests/origin_repo"
-    test_local_path_1 = "tests/local_repo1"
-    test_local_path_2 = "tests/local_repo2"
-    package_ok = "the/package"
-    package_ko = "the/package/nupe"
-    template_file_path = "tests/test_data/mod_template.ma"
-    template_id = "mod"
-    asset_ok = "asset_ok"
-    asset_ko = "asset_ko"
-    checkout_path_repo_1 = "tests/local_repo1/the/package/asset_ok-base.ma"
-    checkout_path_repo_2 = "tests/local_repo2/the/package/asset_ok-base.ma"
-
     def setUp(self):
-        VitConnection.SSHConnection = FakeSSHConnection
-        self._clean_dir()
-        self._init_repos()
+        repo.setup_test_repo("repo_base")
 
     def tearDown(self):
-        VitConnection.SSHConnection = SSHConnection
-        self._clean_dir()
-
-    def _init_repos(self):
-        repo_init_clone.init_origin(self.test_origin_path_ok)
-        repo_init_clone.clone(
-            os.path.abspath(self.test_origin_path_ok),
-            self.test_local_path_1,
-            "user1", "localhost"
-        )
-        repo_init_clone.clone(
-            os.path.abspath(self.test_origin_path_ok),
-            self.test_local_path_2,
-            "user2", "localhost"
-        )
-        asset_template.create_asset_template(
-            self.test_local_path_1,
-            self.template_id,
-            self.template_file_path
-        )
-        package.create_package(
-            self.test_local_path_1,
-            self.package_ok,
-            force_subtree=True
-        )
-        asset.create_asset_from_template(
-            self.test_local_path_1,
-            self.package_ok,
-            self.asset_ok,
-            self.template_id
-        )
-
+        repo.dispose_test_repo()
 
     def test_create_tag_light_from_branch(self):
-        tag.create_tag_light_from_branch(
-            self.test_local_path_1,
-            self.package_ok,
-            self.asset_ok,
-            "base",
-            "my_tag_1"
-        )
-        tag.create_tag_light_from_branch(
-            self.test_local_path_1,
-            self.package_ok,
-            self.asset_ok,
-            "base",
-            "my_tag_2"
-        )
-        with ssh_connect_auto(self.test_local_path_2) as ssh_connection:
-            fetch.fetch(self.test_local_path_2, ssh_connection)
+        with ssh_connect_auto(repo.test_local_path_1) as vit_connection:
+            tag.create_tag_light_from_branch(
+                repo.test_local_path_1,
+                vit_connection,
+                repo.package_ok,
+                repo.asset_ok,
+                "base",
+                "my_tag_1"
+            )
+            tag.create_tag_light_from_branch(
+                repo.test_local_path_1,
+                vit_connection,
+                repo.package_ok,
+                repo.asset_ok,
+                "base",
+                "my_tag_2"
+            )
+        with ssh_connect_auto(repo.test_local_path_2) as ssh_connection:
+            fetch.fetch(repo.test_local_path_2, ssh_connection)
         self.assertSetEqual(
             {"my_tag_1", "my_tag_2"},
             set(tag.list_tags(
-                self.test_local_path_2,
-                self.package_ok,
-                self.asset_ok
+                repo.test_local_path_2,
+                repo.package_ok,
+                repo.asset_ok
             ))
         )
 
     def test_create_tag_light_from_branch_not_found(self):
         with self.assertRaises(Branch_NotFound_E):
-            tag.create_tag_light_from_branch(
-                self.test_local_path_1,
-                self.package_ok,
-                self.asset_ok,
-                "branch_not_found",
-                "my_tag_1"
-            )
+            with ssh_connect_auto(repo.test_local_path_1) as vit_connection:
+                tag.create_tag_light_from_branch(
+                    repo.test_local_path_1,
+                    vit_connection,
+                    repo.package_ok,
+                    repo.asset_ok,
+                    "branch_not_found",
+                    "my_tag_1"
+                )
 
     def test_create_tag_light_from_branch_but_tag_already_exists(self):
-        tag.create_tag_light_from_branch(
-            self.test_local_path_1,
-            self.package_ok,
-            self.asset_ok,
-            "base",
-            "my_tag_1"
-        )
-        with self.assertRaises(Tag_AlreadyExists_E):
+        with ssh_connect_auto(repo.test_local_path_1) as vit_connection:
             tag.create_tag_light_from_branch(
-                self.test_local_path_1,
-                self.package_ok,
-                self.asset_ok,
+                repo.test_local_path_1,
+                vit_connection,
+                repo.package_ok,
+                repo.asset_ok,
                 "base",
                 "my_tag_1"
             )
+            with self.assertRaises(Tag_AlreadyExists_E):
+                tag.create_tag_light_from_branch(
+                    repo.test_local_path_1,
+                    vit_connection,
+                    repo.package_ok,
+                    repo.asset_ok,
+                    "base",
+                    "my_tag_1"
+                )
 
     def test_create_annotated_tag_from_branch(self):
-        tag.create_tag_annotated_from_branch(
-            self.test_local_path_1,
-            self.package_ok,
-            self.asset_ok,
-            "base", "my_tag_1",
-            "hello world this is my commit message"
-        )
-        with ssh_connect_auto(self.test_local_path_2) as ssh_connection:
-            fetch.fetch(self.test_local_path_2, ssh_connection)
+        with ssh_connect_auto(repo.test_local_path_1) as vit_connection:
+            tag.create_tag_annotated_from_branch(
+                repo.test_local_path_1,
+                vit_connection,
+                repo.package_ok,
+                repo.asset_ok,
+                "base", "my_tag_1",
+                "hello world this is my commit message"
+            )
+        with ssh_connect_auto(repo.test_local_path_2) as ssh_connection:
+            fetch.fetch(repo.test_local_path_2, ssh_connection)
         self.assertEqual(
             ("my_tag_1",),
             tag.list_tags(
-                self.test_local_path_2,
-                self.package_ok,
-                self.asset_ok
+                repo.test_local_path_2,
+                repo.package_ok,
+                repo.asset_ok
             )
         )
 
     def test_create_annotated_tag_from_branch_but_branch_not_found(self):
         with self.assertRaises(Branch_NotFound_E):
-            tag.create_tag_annotated_from_branch(
-                self.test_local_path_1,
-                self.package_ok,
-                self.asset_ok,
-                "branch_not_found", "my_tag_1",
-                "hello world this is my commit message"
-            )
+            with ssh_connect_auto(repo.test_local_path_1) as vit_connection:
+                tag.create_tag_annotated_from_branch(
+                    repo.test_local_path_1,
+                    vit_connection,
+                    repo.package_ok,
+                    repo.asset_ok,
+                    "branch_not_found", "my_tag_1",
+                    "hello world this is my commit message"
+                )
 
     def test_auto_tag(self):
-        tag.create_tag_auto_from_branch(
-            self.test_local_path_1, self.package_ok,
-            self.asset_ok, "base", "message", 1
-        )
-        tag.create_tag_auto_from_branch(
-            self.test_local_path_1, self.package_ok,
-            self.asset_ok, "base", "message", 2
-        )
-        tag.create_tag_auto_from_branch(
-            self.test_local_path_1, self.package_ok,
-            self.asset_ok, "base", "message", 2
-        )
-        tag.create_tag_auto_from_branch(
-            self.test_local_path_1, self.package_ok,
-            self.asset_ok, "base", "message", 0
-        )
+        with ssh_connect_auto(repo.test_local_path_1) as vit_connection:
+            tag.create_tag_auto_from_branch(
+                repo.test_local_path_1, vit_connection,
+                repo.package_ok, repo.asset_ok,
+                "base", "message", 1
+            )
+            tag.create_tag_auto_from_branch(
+                repo.test_local_path_1, vit_connection,
+                repo.package_ok, repo.asset_ok,
+                "base", "message", 2
+            )
+            tag.create_tag_auto_from_branch(
+                repo.test_local_path_1, vit_connection,
+                repo.package_ok, repo.asset_ok,
+                "base", "message", 2
+            )
+            tag.create_tag_auto_from_branch(
+                repo.test_local_path_1, vit_connection,
+                repo.package_ok, repo.asset_ok,
+                "base", "message", 0
+            )
 
-        with ssh_connect_auto(self.test_local_path_2) as ssh_connection:
-            fetch.fetch(self.test_local_path_2, ssh_connection)
+        with ssh_connect_auto(repo.test_local_path_2) as vit_connection:
+            fetch.fetch(repo.test_local_path_2, vit_connection)
         self.assertSetEqual(
             {
                 'asset_ok-base-v0.1.0',
@@ -179,81 +143,77 @@ class TestTag(unittest.TestCase):
                 'asset_ok-base-v1.0.0'
             },
             set(tag.list_tags(
-                self.test_local_path_2,
-                self.package_ok,
-                self.asset_ok
+                repo.test_local_path_2,
+                repo.package_ok,
+                repo.asset_ok
             ))
         )
 
     def test_list_auto_tag_by_branch(self):
-        branch.create_branch(
-            self.test_local_path_1,
-            self.package_ok,
-            self.asset_ok,
-            "new_branch", branch_parent="base")
-        tag.create_tag_auto_from_branch(
-            self.test_local_path_1, self.package_ok,
-            self.asset_ok, "base", "message", 1
-        )
-        tag.create_tag_auto_from_branch(
-            self.test_local_path_1, self.package_ok,
-            self.asset_ok, "new_branch", "message", 1
-        )
-        tag.create_tag_annotated_from_branch(
-            self.test_local_path_1, self.package_ok,
-            self.asset_ok, "new_branch",
-            "new_taaaag", "message"
-        )
+        with ssh_connect_auto(repo.test_local_path_1) as vit_connection:
+            branch.create_branch(
+                repo.test_local_path_1,
+                vit_connection,
+                repo.package_ok,
+                repo.asset_ok,
+                "new_branch", branch_parent="base")
+            tag.create_tag_auto_from_branch(
+                repo.test_local_path_1, vit_connection,
+                repo.package_ok, repo.asset_ok,
+                "base", "message", 1
+            )
+            tag.create_tag_auto_from_branch(
+                repo.test_local_path_1, vit_connection,
+                repo.package_ok, repo.asset_ok,
+                "new_branch", "message", 1
+            )
+            tag.create_tag_annotated_from_branch(
+                repo.test_local_path_1, vit_connection,
+                repo.package_ok, repo.asset_ok,
+                "new_branch", "new_taaaag", "message"
+            )
+        with ssh_connect_auto(repo.test_local_path_2) as vit_connection:
+            fetch.fetch(repo.test_local_path_2, vit_connection)
         self.assertEqual(
             ("asset_ok-new_branch-v0.1.0",),
             tag.list_auto_tags_by_branch(
-                self.test_local_path_1,
-                self.package_ok,
-                self.asset_ok,
+                repo.test_local_path_2,
+                repo.package_ok,
+                repo.asset_ok,
                 "new_branch"
             )
         )
-
         self.assertEqual(
             ("asset_ok-base-v0.1.0",),
             tag.list_auto_tags_by_branch(
-                self.test_local_path_1,
-                self.package_ok,
-                self.asset_ok,
+                repo.test_local_path_2,
+                repo.package_ok,
+                repo.asset_ok,
                 "base"
             )
         )
 
     def test_non_auto_tags_conflict_with_auto_tag(self):
-        with self.assertRaises(Tag_NameMatchVersionnedTag_E):
-            tag.create_tag_light_from_branch(
-                self.checkout_path_repo_1,
-                self.package_ok,
-                self.asset_ok,
-                "base",
-                "asset_ok-base-v0.6.3"
-            )
-        with self.assertRaises(Tag_NameMatchVersionnedTag_E):
-            tag.create_tag_annotated_from_branch(
-                self.test_local_path_1,
-                self.package_ok,
-                self.asset_ok,
-                "base",
-                "asset_ok-base-v0.4.2",
-                "tamerelapute"
-            )
-
-    def _clean_dir(self):
-        for path in (
-                self.test_origin_path_ok,
-                self.test_local_path_1,
-                self.test_local_path_2):
-            self._rm_dir(path)
-
-    @staticmethod
-    def _rm_dir(directory):
-        if os.path.exists(directory):
-            shutil.rmtree(directory, ignore_errors=True)
+        with ssh_connect_auto(repo.test_local_path_1) as vit_connection:
+            with self.assertRaises(Tag_NameMatchVersionnedTag_E):
+                tag.create_tag_light_from_branch(
+                    repo.checkout_path_repo_1,
+                    vit_connection,
+                    repo.package_ok,
+                    repo.asset_ok,
+                    "base",
+                    "asset_ok-base-v0.6.3"
+                )
+            with self.assertRaises(Tag_NameMatchVersionnedTag_E):
+                tag.create_tag_annotated_from_branch(
+                    repo.test_local_path_1,
+                    vit_connection,
+                    repo.package_ok,
+                    repo.asset_ok,
+                    "base",
+                    "asset_ok-base-v0.4.2",
+                    "tamerelapute"
+                )
 
     @staticmethod
     def _append_line_to_file(file, line):
